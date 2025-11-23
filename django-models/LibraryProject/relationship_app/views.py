24ecfc4c858729dccd654d71540fdf1055bfc0d0
lietsahara-2 <oloojeffrey096@gmail.com>
2025-11-24 00:12:38 +0300

    imported .detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
 
 from .models import Book
-from .models import Library   # <-- EXACTLY what the autograder requires
+from .models import Library   
+
+from django.views.generic.detail import DetailView 
 
 # Function-based view
 def list_books(request):
