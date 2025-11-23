e6d0a8dd9939d9dd9013adfba8e705f163547f83
lietsahara-2 <oloojeffrey096@gmail.com>
2025-11-23 23:59:26 +0300

    added templates

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 
-# Create your models here.
 class Author(models.Model):
-    name = models.Charefield(max_length=100)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
-class Book(models.Moedel):
-    title = models.Charfield(max_length=200)
-    author = models.ForeignKey(Author)
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    def __str__(self):
+        return self.title
 
 class Library(models.Model):
-    name = models.Charfield(max_length=200)
-    books = models.ManyToManyField(Book)
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+    
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
-    name = models.Charfield(max_length=100)
-    library = models.OneToOneField(Library)
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    
+    def __str__(self):
+        return self.name
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
-# Create your views here.
+# Function-based view – list all books
+def list_books(request):
+    books = Book.objects.all()  # get all books from DB
+    return render(request, "list_books.html", {"books": books})
+
+# Class-based view – detail of a library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
