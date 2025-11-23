f6efa61af1192db56b94cbd53e0afa3fc3f7fe71
lietsahara-2 <oloojeffrey096@gmail.com>
2025-11-24 00:04:14 +0300

    changed the return function in views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.views.generic import DetailView
 # Function-based view – list all books
 def list_books(request):
     books = Book.objects.all()  # get all books from DB
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 # Class-based view – detail of a library
 class LibraryDetailView(DetailView):
